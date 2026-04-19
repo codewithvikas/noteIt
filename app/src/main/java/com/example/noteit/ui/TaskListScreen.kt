e9ceaeb6797fa30fd5e01cfa67856f20ffef086f
vikas kumar jha <vikasjhag@gmail.com>
2026-04-19 21:19:01 +0530

    add task functionality

--- app/src/main/java/com/example/noteit/ui/TaskListScreen.kt
+++ app/src/main/java/com/example/noteit/ui/TaskListScreen.kt
@@ -1,13 +1,23 @@
 package com.example.noteit.ui
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.compose.LocalLifecycleOwner
@@ -17,13 +27,25 @@ import com.example.noteit.data.TaskListRepository
 import com.example.noteit.ui.theme.NoteItTheme
 
 @Composable
-fun TaskListScreen(modifier: Modifier) {
+fun TaskListScreen() {
+    val viewModelStoreOwner = LocalLifecycleOwner.current as ViewModelStoreOwner
+    Log.d("ViewModelStoreOwner:", "${viewModelStoreOwner.viewModelStore.keys()}")
+    viewModelStoreOwner.viewModelStore
     val viewModel = ViewModelProvider(
-        owner = LocalLifecycleOwner.current as ViewModelStoreOwner,
+        owner = viewModelStoreOwner,
         factory = TaskListViewModelFactory(TaskListRepository())
-    ).get(TaskListViewModel::class.java)
+    ).get("${Math.random().times(100)}", TaskListViewModel::class.java)
+    Log.d("viewModel", "$viewModel")
     val tasks = viewModel.tasks.collectAsStateWithLifecycle()
-    TaskList(tasks.value, modifier)
+    Scaffold(modifier = Modifier.fillMaxSize(), floatingActionButton = {
+        Button(onClick = {
+            viewModel.addTask(Task("New Task", "New Description"))
+        }) {
+            Text(text = "Add Task")
+        }
+    }) { innerPadding ->
+        TaskList(tasks = tasks.value, modifier = Modifier.padding(innerPadding))
+    }
 
 }
 
@@ -38,9 +60,19 @@ fun TaskList(tasks: List<Task>, modifier: Modifier = Modifier) {
 
 @Composable
 fun TaskItem(task: Task) {
-    Column {
-        Text(text = "Title:${task.title}")
-        Text(text = "Description:${task.description}")
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(8.dp)
+    ) {
+        Column(modifier = Modifier.padding(8.dp)) {
+            Text(
+                text = task.title,
+                style = TextStyle(fontSize = 14.sp, fontWeight = FontWeight.Bold)
+            )
+            Text(text = task.description, style = TextStyle(fontSize = 12.sp))
+        }
     }
 }
 
