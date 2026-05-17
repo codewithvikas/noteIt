c65af30cd07fc013e93b76566bce333328f58b47
vikas kumar jha <vikasjhag@gmail.com>
2026-05-17 11:33:19 +0530

    fixed task screen todo

--- app/src/main/java/com/example/noteit/tasks/TasksScreen.kt
+++ app/src/main/java/com/example/noteit/tasks/TasksScreen.kt
@@ -1,21 +1,17 @@
 package com.example.noteit.tasks
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelStoreOwner
-import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.example.noteit.data.DefaultTaskRepository
-import com.example.noteit.data.source.local.TaskDao
-import com.example.noteit.data.source.local.ToDoDatabase
 
 @Composable
-fun TasksScreen( viewModel: TasksViewModel
+fun TasksScreen(
+    viewModel: TasksViewModel
 ) {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -29,5 +25,9 @@ fun TasksScreen( viewModel: TasksViewModel
 
 @Composable
 fun TaskContent(tasks: TaskUiState, modifier: Modifier = Modifier) {
-    Text("Helo")
+    Column(modifier = modifier) {
+        for (item in tasks.items) {
+            Text(text = item.title)
+        }
+    }
 }
